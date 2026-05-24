5ec16da517bb8dddd7e77f9875692c4f280083be
Arjun RJ <arjunrj.1701@gmail.com>
2026-05-24 23:47:05 +0530

    Fixed nodemailer config

--- backend/server.js
+++ backend/server.js
@@ -58,15 +58,6 @@ const otpStorage = {};
 
 const users = [];
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 /*
 ==================================================
 SEND OTP
