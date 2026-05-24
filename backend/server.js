7810cbfe8bc2807c1ef29a0013a721d1e4d8a2f2
Arjun RJ <arjunrj.1701@gmail.com>
2026-05-25 01:28:53 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -63,10 +63,22 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
 });
 
 /*
