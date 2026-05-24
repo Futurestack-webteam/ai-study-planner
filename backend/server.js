ebb73538841896af0cf649d89d2dcf8a71130e58
Arjun RJ <arjunrj.1701@gmail.com>
2026-05-25 00:28:06 +0530

    Configured Brevo SMTP

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,9 @@ const users = [];
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
